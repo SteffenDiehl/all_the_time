2be8695282e9f90e8d43d2351c42257ec61ae990
Ogham <ChristophGrell.CG@gmail.com>
2023-06-16 11:47:39 +0200

    CG

--- all_the_time/src/Day_Time.h
+++ all_the_time/src/Day_Time.h
@@ -1,53 +1,24 @@
 #include <WiFi.h>
-
+#include <Wire.h>
 #include <WiFiUdp.h>
-
+#include "RTClib.h"
 #include <time.h>
 
 const char* ntpServer = "de.pool.ntp.org";
 const long  gmtOffset_sec = 0;
 const int   daylightOffset_sec = 3600;
 
-void start_RTC(){
-  return;
+void start_RTC()
+{
+  Wire.begin();
+  rtc.begin();
+
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
-void printLocalTime(){
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return;
-  }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-  Serial.print("Day of week: ");
-  Serial.println(&timeinfo, "%A");
-  Serial.print("Month: ");
-  Serial.println(&timeinfo, "%B");
-  Serial.print("Day of Month: ");
-  Serial.println(&timeinfo, "%d");
-  Serial.print("Year: ");
-  Serial.println(&timeinfo, "%Y");
-  Serial.print("Hour: ");
-  Serial.println(&timeinfo, "%H");
-  Serial.print("Hour (12 hour format): ");
-  Serial.println(&timeinfo, "%I");
-  Serial.print("Minute: ");
-  Serial.println(&timeinfo, "%M");
-  Serial.print("Second: ");
-  Serial.println(&timeinfo, "%S");
+void ac_time(int *Y, int *M, int *D, int *h, int *m, int *s)
+{
+  DateTime now = rtc.now();
 
-  Serial.println("Time variables");
-  char timeHour[3];
-  strftime(timeHour,3, "%H", &timeinfo);
-  Serial.println(timeHour);
-  char timeWeekDay[10];
-  strftime(timeWeekDay,10, "%A", &timeinfo);
-  Serial.println(timeWeekDay);
-  Serial.println();
-}
 
-void get_time(){
-    // Init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
 }
